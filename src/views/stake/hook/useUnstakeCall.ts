f08b514c0100ed7684c56f79a0f8640626245345
shenger <714275326@qq.com>
2022-05-30 10:53:08 +0800

    fix

--- src/views/stake/hook/useUnstakeCall.ts
+++ src/views/stake/hook/useUnstakeCall.ts
@@ -83,8 +83,8 @@ function calculateRedeem(
     unstakeFee: redeemResult.fee,
     params: {
       network,
-      method: 'request_redeem_complately',
-      section: 'batch_all',
+      method: 'batchAll',
+      section: 'utility',
       params: [
         [api.tx.homa.requestRedeem(redeemResult.request.toChainData(), true), api.tx.homa.fastMatchRedeemsCompletely([current])],
       ],
